3bb4c2f446153e285f70b27d7c4802a9be8bd424
corcor27 <cory13579@googlemail.com>
2017-10-24 20:16:42 +0100

    Update subplot.py

--- subplot.py
+++ subplot.py
@@ -17,7 +17,7 @@ def chi_p_1():
         data.append([float(x) for x in line.split()])
     Chi_p_1 = [x[52] for x in data ]
     return Chi_p_1 
-    
+
 def chi_p_2():
     data = []
     g = open('M40_INC_10.0.txt', 'r')
@@ -33,22 +33,22 @@ def chi_p_3():
         data.append([float(x) for x in line.split()])
     Chi_p_3 = [x[52] for x in data ]
     return Chi_p_3 
-    
+
 def chi_p_4():
     data = []
     g = open('M40_INC_30.0.txt', 'r')
     for line in g:
         data.append([float(x) for x in line.split()])
     Chi_p_4 = [x[52] for x in data ]
-    return Chi_p_4     
-    
+    return Chi_p_4 
+
 def chi_p_5():
     data = []
     g = open('M40_INC_40.0.txt', 'r')
     for line in g:
         data.append([float(x) for x in line.split()])
     Chi_p_5 = [x[52] for x in data ]
-    return Chi_p_5     
+    return Chi_p_5    
 
 def chi_p_6():
     data = []
@@ -57,7 +57,7 @@ def chi_p_6():
         data.append([float(x) for x in line.split()])
     Chi_p_6 = [x[52] for x in data ]
     return Chi_p_6 
-    
+
 def chi_p_7():
     data = []
     g = open('M40_INC_60.0.txt', 'r')
@@ -65,29 +65,34 @@ def chi_p_7():
         data.append([float(x) for x in line.split()])
     Chi_p_7 = [x[52] for x in data ]
     return Chi_p_7    
-    
+
 chi_p_INC_0 = chi_p_1()
 INC_0_upper_90=np.percentile(chi_p_INC_0, 95)
 INC_0_lower_90=np.percentile(chi_p_INC_0, 5)
+
 chi_p_INC_10 = chi_p_2()
 INC_10_upper_90=np.percentile(chi_p_INC_10, 95)
 INC_10_lower_90=np.percentile(chi_p_INC_10, 5)
+
 chi_p_INC_20 = chi_p_3()
 INC_20_upper_90=np.percentile(chi_p_INC_20, 95)
 INC_20_lower_90=np.percentile(chi_p_INC_20, 5)
+
 chi_p_INC_30 = chi_p_4()
 INC_30_upper_90=np.percentile(chi_p_INC_30, 95)
 INC_30_lower_90=np.percentile(chi_p_INC_30, 5)
+
 chi_p_INC_40 = chi_p_5()
 INC_40_upper_90=np.percentile(chi_p_INC_40, 95)
 INC_40_lower_90=np.percentile(chi_p_INC_40, 5)
+
 chi_p_INC_50 = chi_p_6()
 INC_50_upper_90=np.percentile(chi_p_INC_50, 95)
 INC_50_lower_90=np.percentile(chi_p_INC_50, 5)
+
 chi_p_INC_60 = chi_p_7()
 INC_60_upper_90=np.percentile(chi_p_INC_60, 95)
 INC_60_lower_90=np.percentile(chi_p_INC_60, 5)
-
 
 plt.hist(chi_p_INC_0,50, facecolor='m', normed=True)
 #plt.hist(pycbc_data,50, normed=True, color='b')
@@ -96,7 +101,7 @@ plt.xlabel('chi_p')
 #plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
 #plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
 #plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
-plt.axvline(x=0.75,linewidth=2, color='r')
+plt.axvline(x=inj_v,linewidth=2, color='r')
 plt.ylabel('probability density')
 plt.savefig("chi_mix_plot.png")
 
