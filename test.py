61d786fb436f0b1449485ea207e0c5f486381ca1
corcor27 <cory13579@googlemail.com>
2017-10-24 19:25:02 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+print "Initialising..."
+
+inj_v=0.75
+
+#manually set injection value
+
+def chi_p_1():
+    data = []
+    g = open('M40_INC_0.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_1 = [x[52] for x in data ]
+    return Chi_p_1 
+    
+chi_p_INC_0 = chi_p_1()
+INC_0_upper_90=np.percentile(chi_p_INC_0, 95)
+INC_0_lower_90=np.percentile(chi_p_INC_0, 5)
+plt.hist(chi_p_INC_0,50, facecolor='m', normed=True)
+#plt.hist(pycbc_data,50, normed=True, color='b')
+plt.xlabel('chi_p')
+#plt.axvline(x=Lal_lower_90,linewidth=2,linestyle='dashed',color='m')
+#plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
+#plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
+#plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
+plt.axvline(x=0.75,linewidth=2, color='r')
+plt.ylabel('probability density')
+plt.savefig("chi_mix_plot.png")
