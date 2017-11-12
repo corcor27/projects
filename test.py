1dd91ef63ff99dd96318ba86b20a65e5a073076d
corcor27 <cory13579@googlemail.com>
2017-11-12 23:38:15 +0000

    Update test.py

--- test.py
+++ test.py
@@ -66,6 +66,22 @@ def chi_p_7():
     Chi_p_7 = [x[52] for x in data ]
     return Chi_p_7    
 
+def chi_p_8():
+    data = []
+    g = open('M40_INC_70.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_6 = [x[52] for x in data ]
+    return Chi_p_6 
+
+def chi_p_9():
+    data = []
+    g = open('M40_INC_80.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_7 = [x[52] for x in data ]
+    return Chi_p_7    
+
 chi_p_INC_0 = chi_p_1()
 INC_0_upper_90=np.percentile(chi_p_INC_0, 95)
 INC_0_lower_90=np.percentile(chi_p_INC_0, 5)
@@ -94,14 +110,29 @@ chi_p_INC_60 = chi_p_7()
 INC_60_upper_90=np.percentile(chi_p_INC_60, 95)
 INC_60_lower_90=np.percentile(chi_p_INC_60, 5)
 
+chi_p_INC_70 = chi_p_8()
+INC_50_upper_90=np.percentile(chi_p_INC_50, 95)
+INC_50_lower_90=np.percentile(chi_p_INC_50, 5)
+
+chi_p_INC_80 = chi_p_9()
+INC_60_upper_90=np.percentile(chi_p_INC_60, 95)
+INC_60_lower_90=np.percentile(chi_p_INC_60, 5)
+
 plt.hist(chi_p_INC_0,50, facecolor='m', normed=True)
 plt.hist(chi_p_INC_10,50, facecolor='g', normed=True)
+plt.hist(chi_p_INC_20,50, facecolor='b', normed=True)
+plt.hist(chi_p_INC_30,50, facecolor='c', normed=True)
+plt.hist(chi_p_INC_40,50, facecolor='y', normed=True)
+plt.hist(chi_p_INC_50,50, facecolor='r', normed=True)
+plt.hist(chi_p_INC_60,50, facecolor='m', normed=True)
+plt.hist(chi_p_INC_70,50, facecolor='g', normed=True)
+plt.hist(chi_p_INC_80,50, facecolor='b', normed=True)
 #plt.hist(pycbc_data,50, normed=True, color='b')
 plt.xlabel('chi_p')
 #plt.axvline(x=Lal_lower_90,linewidth=2,linestyle='dashed',color='m')
 #plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
 #plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
 #plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
-plt.axvline(x=inj_v,linewidth=2, color='r')
+#plt.axvline(x=inj_v,linewidth=2, color='r')
 plt.ylabel('probability density')
-plt.savefig("test_plot.png")
+plt.savefig("test_plot1.png")
