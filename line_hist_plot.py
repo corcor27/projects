e4c290ecef9378d17ce399aa23c3520b98551614
corcor27 <cory13579@googlemail.com>
2017-11-13 01:18:38 +0000

    Update line_hist_plot.py

--- line_hist_plot.py
+++ line_hist_plot.py
@@ -198,6 +198,119 @@ def chi_p_9_90PE():
     mean_val_9=np.average(saved_chi_p_9)
     PE_9 = (abs(upper_90_9 - injected_value) / injected_value) * 100
     return PE_9
+
+inj_v=0.75
+
+#manually set injection value
+
+def chi_p_1():
+    data = []
+    g = open('M40_INC_0.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_1 = [x[52] for x in data ]
+    return Chi_p_1 
+
+def chi_p_2():
+    data = []
+    g = open('M40_INC_10.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_2 = [x[52] for x in data ]
+    return Chi_p_2     
+
+def chi_p_3():
+    data = []
+    g = open('M40_INC_20.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_3 = [x[52] for x in data ]
+    return Chi_p_3 
+
+def chi_p_4():
+    data = []
+    g = open('M40_INC_30.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_4 = [x[52] for x in data ]
+    return Chi_p_4 
+
+def chi_p_5():
+    data = []
+    g = open('M40_INC_40.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_5 = [x[52] for x in data ]
+    return Chi_p_5    
+
+def chi_p_6():
+    data = []
+    g = open('M40_INC_50.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_6 = [x[52] for x in data ]
+    return Chi_p_6 
+
+def chi_p_7():
+    data = []
+    g = open('M40_INC_60.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_7 = [x[52] for x in data ]
+    return Chi_p_7    
+
+def chi_p_8():
+    data = []
+    g = open('M40_INC_70.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_6 = [x[52] for x in data ]
+    return Chi_p_6 
+
+def chi_p_9():
+    data = []
+    g = open('M40_INC_80.0.txt', 'r')
+    for line in g:
+        data.append([float(x) for x in line.split()])
+    Chi_p_7 = [x[52] for x in data ]
+    return Chi_p_7    
+
+chi_p_INC_0 = chi_p_1()
+INC_0_upper_90=np.percentile(chi_p_INC_0, 95)
+INC_0_lower_90=np.percentile(chi_p_INC_0, 5)
+
+chi_p_INC_10 = chi_p_2()
+INC_10_upper_90=np.percentile(chi_p_INC_10, 95)
+INC_10_lower_90=np.percentile(chi_p_INC_10, 5)
+
+chi_p_INC_20 = chi_p_3()
+INC_20_upper_90=np.percentile(chi_p_INC_20, 95)
+INC_20_lower_90=np.percentile(chi_p_INC_20, 5)
+
+chi_p_INC_30 = chi_p_4()
+INC_30_upper_90=np.percentile(chi_p_INC_30, 95)
+INC_30_lower_90=np.percentile(chi_p_INC_30, 5)
+
+chi_p_INC_40 = chi_p_5()
+INC_40_upper_90=np.percentile(chi_p_INC_40, 95)
+INC_40_lower_90=np.percentile(chi_p_INC_40, 5)
+
+chi_p_INC_50 = chi_p_6()
+INC_50_upper_90=np.percentile(chi_p_INC_50, 95)
+INC_50_lower_90=np.percentile(chi_p_INC_50, 5)
+
+chi_p_INC_60 = chi_p_7()
+INC_60_upper_90=np.percentile(chi_p_INC_60, 95)
+INC_60_lower_90=np.percentile(chi_p_INC_60, 5)
+
+chi_p_INC_70 = chi_p_8()
+INC_50_upper_90=np.percentile(chi_p_INC_50, 95)
+INC_50_lower_90=np.percentile(chi_p_INC_50, 5)
+
+chi_p_INC_80 = chi_p_9()
+INC_60_upper_90=np.percentile(chi_p_INC_60, 95)
+INC_60_lower_90=np.percentile(chi_p_INC_60, 5)
+
 # recovered values from functions 
 
 run1MPE = chi_p_1_MPE()
@@ -231,22 +344,46 @@ ax1.errorbar(x=Incliations, y=a, yerr=ae, fmt='o', color='g', label = 'Incliatio
 ax1.legend(loc='lower right', fontsize=10.5)
 ax1.axis([-10, 90, -15, 25])
 ax1.set_title('Inclination_scatter')
-ax1.set_ylabel('Precentage error (%)', fontsize=12)
+ax1.set_ylabel('Precentage error chi_p (%)', fontsize=12)
 ax1.set_xlabel('Inclination (Degrees)', fontsize=12)
 
 ax2 = fig.add_subplot(222)
 ax2.plot(Incliations, a, linewidth=2,linestyle='dashed',color='g', label = 'Incliation ranges')
 ax2.axis([-10, 90, 0, 9])
 ax2.set_title('Inclination_line')
-ax2.set_ylabel('Precentage error (%)', fontsize=12)
+ax2.set_ylabel('Precentage error chi_p (%)', fontsize=12)
 ax2.set_xlabel('Inclination (Degrees)', fontsize=12)
+
+ax3 = = fig.add_subplot(222)
+ax3.hist(chi_p_INC_0,50, facecolor='m', normed=True, label = '0 deg')
+ax3.hist(chi_p_INC_10,50, facecolor='g', normed=True, label = '10 deg')
+ax3.hist(chi_p_INC_20,50, facecolor='b', normed=True, label = '20 deg')
+ax3.hist(chi_p_INC_30,50, facecolor='c', normed=True, label = '30 deg')
+ax3.hist(chi_p_INC_40,50, facecolor='y', normed=True, label = '40 deg')
+ax3.hist(chi_p_INC_50,50, facecolor='grey', normed=True, label = '50 deg')
+ax3.hist(chi_p_INC_60,50, facecolor='lightgreen', normed=True, label = '60 deg')
+ax3.hist(chi_p_INC_70,50, facecolor='skyblue', normed=True, label = '70 deg')
+ax3.hist(chi_p_INC_80,50, facecolor='lightcoral', normed=True, label = '80 deg')
+#plt.hist(pycbc_data,50, normed=True, color='b')
+ax3.xlabel('chi_p')
+#plt.axvline(x=Lal_lower_90,linewidth=2,linestyle='dashed',color='m')
+#plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
+#plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
+#plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
+ax3.axvline(x=inj_v,linewidth=2, color='r')
+ax3.ylabel('probability density')
+ax3.legend(loc='upper left', fontsize=10.5)
+
+
 
 fig.tight_layout()
 fig.set_figheight(5)
 fig.set_figwidth(10)
 
 
+
+
 # Set common labels
 
 
-plt.savefig("Inc_test11.png")
+plt.savefig("Inc_his1.png")
